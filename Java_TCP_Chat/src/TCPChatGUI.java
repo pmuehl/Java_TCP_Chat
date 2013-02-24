12bbe8f348895f05af14243b83d25aa4db90930d
Patrick Muehl <pmuehl@student.tgm.ac.at>
2013-02-24 15:21:41 +0100

    Gui Verbessert

--- Java_TCP_Chat/src/TCPChatGUI.java
+++ Java_TCP_Chat/src/TCPChatGUI.java
@@ -1,3 +1,11 @@
+import java.awt.event.KeyEvent;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
+
 /**
  * 
  */
@@ -7,7 +15,31 @@
  *
  */
 public class TCPChatGUI {
+	private JButton connect;
+	private JRadioButton server;
+	private JRadioButton client;
+	private JButton send;
+	private JLabel txt;
+	private JTextArea message;
+	
+	
 	public TCPChatGUI() {
+		connect= new JButton("Connect");
+		send= new JButton("Nachricht schicken");
+		txt= new JLabel("");
+		message= new JTextArea("Geben sie hier ihre Nachricht ein");
+		
+		ButtonGroup bg = new ButtonGroup();
+	    server = new JRadioButton("Server", true);
+	    server.setMnemonic(KeyEvent.VK_H);
+	    server.setActionCommand("server");
 
+	    client = new JRadioButton("Client", false);
+	    client.setMnemonic(KeyEvent.VK_G);
+	    client.setActionCommand("client");
+
+	    bg.add(server);
+	    bg.add(client);
+	    
 	}
 }
