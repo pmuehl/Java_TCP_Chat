612b8af0dcd7330832591723757d220a484de71d
Patrick Muehl <pmuehl@student.tgm.ac.at>
2013-03-02 11:34:44 +0100

    Tests durchgeführt. MyFrame aus der main methode gelöscht

--- Java_TCP_Chat/src/Chat.java
+++ Java_TCP_Chat/src/Chat.java
@@ -6,7 +6,7 @@
 public class Chat {
 	public static void main (String [] args){
 
-//		MyFrame f= new MyFrame();
+
 		//ChatLogic c = new ChatLogic("Patrick");
 		TCPChatSwtGui b = new TCPChatSwtGui();
 
