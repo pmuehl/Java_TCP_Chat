5170fceeed4c2b7e366c43b23e5f87367476056c
Patrick Muehl <pmuehl@student.tgm.ac.at>
2013-03-04 19:49:04 +0100

    einige funktionen eingebaut

--- Java_TCP_Chat/src/TCPChatSwtGui.java
+++ Java_TCP_Chat/src/TCPChatSwtGui.java
@@ -37,57 +37,47 @@ public class TCPChatSwtGui {
 	private Text text_1;
 	private Text text_2;
 	private Text text_3;
-<<<<<<< HEAD
-
-	private String t="";
-
-
-
-=======
-	private String t=""+"\n";
->>>>>>> 579be463d061d326c3fc6855da0e9a027735a767
+	private String t="";	
 	private String uname="";
+	private boolean pruefe=false;
+
+
+
 	public TCPChatSwtGui() {
 		Display dis = new Display();
 		xp = new Shell(dis);		
 		xp.setSize(458, 505);
-		
-		
+
+
 		final List list = new List(xp, SWT.BORDER);
 		list.setBounds(340, 59, 94, 282);
-		
+
 		text_1 = new Text(xp, SWT.BORDER);
 		text_1.setBounds(10, 348, 312, 58);
-		
-<<<<<<< HEAD
-		Button btnSenden = new Button(xp, SWT.NONE);
-=======
+
+		//Button btnSenden = new Button(xp, SWT.NONE);
 		final Button btnSenden = new Button(xp, SWT.NONE);
->>>>>>> 579be463d061d326c3fc6855da0e9a027735a767
 		btnSenden.setBounds(340, 347, 75, 25);
 		btnSenden.setText("Senden");
 		btnSenden.addSelectionListener(new SelectionListener() {
-			
+
 			public void widgetDefaultSelected(SelectionEvent e) {
 			}
-		    public void widgetSelected(SelectionEvent e) {
-		    	
-		        //System.out.println("Button pushed.");
-		    	t=t+text_1.getText()+"\n";
-<<<<<<< HEAD
-		    	text_3.setText(t);
-=======
-		    	text_3.setText(uname + ": " +t);
->>>>>>> 579be463d061d326c3fc6855da0e9a027735a767
-		    	text_1.setText("");
-		      }
-		   });
-		
+			public void widgetSelected(SelectionEvent e) {
+
+				//System.out.println("Button pushed.");
+				t=t+uname+": "+text_1.getText()+"\n";
+
+				text_3.setText(t);
+				text_1.setText("");
+			}
+		});
+
 		Button btnLogin = new Button(xp, SWT.NONE);
 		btnLogin.setBounds(357, 10, 75, 25);
 		btnLogin.setText("LogIN");
 		btnLogin.addSelectionListener(new SelectionListener(){
-			
+
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
 				// TODO Auto-generated method stub	
@@ -95,31 +85,34 @@ public class TCPChatSwtGui {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				// TODO Auto-generated method stub
-				uname= text_2.getText();
-				list.add(uname);
-<<<<<<< HEAD
-				
-				
-=======
-						
->>>>>>> 579be463d061d326c3fc6855da0e9a027735a767
+				if(pruefe== false){
+					uname= text_2.getText();
+					list.add(uname);
+					text_2.setText("");
+					pruefe= true;
+				}else{
+					text_2.setText("Bereits angemeldet");
+					text_2.setEditable(false);
+				}
+
+
 			}
-			
-			
+
+
 		});
-		
+
 		text_2 = new Text(xp, SWT.BORDER);
 		text_2.setBounds(216, 12, 118, 25);
-		
-		text_3 = new Text(xp, SWT.BORDER);
+
+		text_3 = new Text(xp, SWT.V_SCROLL);
 		text_3.setEditable(false);
 		text_3.setBounds(10, 59, 312, 282);
-		
+
 		Button btnLogOUT = new Button(xp, SWT.NONE);
 		btnLogOUT.setBounds(10, 10, 75, 25);
 		btnLogOUT.setText("LogOUt");
 		btnLogOUT.addSelectionListener(new SelectionListener(){
-			
+
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
 				// TODO Auto-generated method stub	
@@ -129,19 +122,19 @@ public class TCPChatSwtGui {
 				// TODO Auto-generated method stub
 				uname= text_2.getText();
 				list.remove(uname);
-				
-				
+
+
 			}
-			
-			
+
+
 		});
-		
+
 		xp.open();
 		while (!xp.isDisposed()) {
-			   if (!dis.readAndDispatch()) {
-			    dis.sleep();
+			if (!dis.readAndDispatch()) {
+				dis.sleep();
 			}
 		}
 	}
-	
+
 }
