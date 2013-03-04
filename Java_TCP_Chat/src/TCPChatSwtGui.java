916fd07dab3454353298bc9febc4f9f7602904d4
Patrick Muehl <pmuehl@student.tgm.ac.at>
2013-03-04 17:02:36 +0100

    gui erweitert und angepasst

--- Java_TCP_Chat/src/TCPChatSwtGui.java
+++ Java_TCP_Chat/src/TCPChatSwtGui.java
@@ -27,69 +27,81 @@ public class TCPChatSwtGui {
 	private Text text_1;
 	private Text text_2;
 	private Text text_3;
-	private String t=""+"\n";
+	private String t="";;
 	private String uname="";
+	private boolean pruefe= false;
 	public TCPChatSwtGui() {
 		Display dis = new Display();
 		xp = new Shell(dis);		
-		
-		
+
+
 		final List list = new List(xp, SWT.BORDER);
 		list.setBounds(340, 59, 94, 282);
-		
+
 		text_1 = new Text(xp, SWT.BORDER);
 		text_1.setBounds(10, 348, 312, 58);
-		
+
 		Button btnSenden = new Button(xp, SWT.NONE);
 		btnSenden.setBounds(359, 381, 75, 25);
 		btnSenden.setText("Senden");
 		btnSenden.addSelectionListener(new SelectionListener() {
-			
+
 			public void widgetDefaultSelected(SelectionEvent e) {
 			}
-		    public void widgetSelected(SelectionEvent e) {
-		    	
-		        //System.out.println("Button pushed.");
-		    	t=t+text_1.getText()+"\n";
-		    	text_3.setText(t);
-		    	text_1.setText("");
-		      }
-		   });
-		
+			public void widgetSelected(SelectionEvent e) {
+
+				//System.out.println("Button pushed.");
+				t=text_1.getText()+'\n';
+				//text_3.setText(t+'\n');
+				//text_3.redraw();
+				text_3.insert(t);
+				
+				text_1.setText("");
+				
+			}
+		});
+
 		Button btnLogin = new Button(xp, SWT.NONE);
 		btnLogin.setBounds(10, 10, 75, 25);
 		btnLogin.setText("LogIN");
 		btnLogin.addSelectionListener(new SelectionListener(){
-			
+
 			@Override
 			public void widgetDefaultSelected(SelectionEvent e) {
 				// TODO Auto-generated method stub	
 			}
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				
 				// TODO Auto-generated method stub
-				uname= text_2.getText();
-				list.add(uname);
-				
-				
+				if(pruefe== false){
+					uname= text_2.getText();
+					list.add(uname);
+					text_2.setText("");
+					pruefe= true;
+				}else{
+					text_2.setText("Bereits angemeldet");
+					text_2.setEditable(false);
+				}
+
 			}
-			
-			
+
+
 		});
-		
+
 		text_2 = new Text(xp, SWT.BORDER);
 		text_2.setBounds(103, 10, 118, 25);
-		
-		text_3 = new Text(xp, SWT.BORDER);
+
+		text_3 = new Text(xp, SWT.V_SCROLL);
 		text_3.setEditable(false);
 		text_3.setBounds(10, 59, 312, 282);
-		
+
 		xp.open();
 		while (!xp.isDisposed()) {
-			   if (!dis.readAndDispatch()) {
-			    dis.sleep();
-			   }
+			if (!dis.readAndDispatch()) {
+				dis.sleep();
+			}
 		}
 	}
-	
+
 }
